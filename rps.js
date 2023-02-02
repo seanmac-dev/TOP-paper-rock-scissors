0ae2ef66c13b40c3c24b9e334cc2f1b921f3bfb9
seanmac85 <109491703+seanmac-dev@users.noreply.github.com>
2023-02-02 13:11:23 -0500

    Alert computer generated selection

--- rps.js
+++ rps.js
@@ -1,6 +1,5 @@
 let choices = ['Rock', 'Paper', 'Scissors'];
 let weapons = choices[Math.floor(Math.random()*choices.length)];
-function getComputerChoice (weapons) {
-    return "The computer chose: " + weapons
-}
+    alert('The computer chose: ' + weapons);
+
 
