e07848bfa3781902cb09793a57eb7375b0a241bb
seanmac85 <109491703+seanmac-dev@users.noreply.github.com>
2023-02-02 22:41:54 -0500

    Add conditionals for determining winner

--- rps.js
+++ rps.js
@@ -4,3 +4,24 @@ function getComputerChoice(weapons) {
     return 'The computer chose: ' + weapons
 }
 
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function playRound(playerSelection, getComputerChoice) {
+    if (playerSelection === getComputerChoice) 
+      {
+        para.textContent = `It\'s a tie!!`;
+      } else if (playerSelection === 'Rock' && getComputerChoice === 'Scissors') 
+                (playerSelection === 'Scissors' && getComputerChoice === 'Paper') ||
+                (playerSelection === 'Paper' && getComputerChoice === 'Rock') 
+        {
+        para.textContent = `You won!`;
+        playerScore++
+        roundWinner = `You beat the machine!!`
+        }
+    }
+  const playerSelection = prompt();
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, getComputerChoice));
+
