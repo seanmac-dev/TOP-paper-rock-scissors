a04265221f9bca3906b5f76f7c39b779e6fbf024
seanmac85 <109491703+seanmac-dev@users.noreply.github.com>
2023-02-04 22:13:31 -0500

    Major overhaul of game function
    Change to using switch
    Randomized computer response is accurate
    Issue with playerSelection

--- rps.js
+++ rps.js
@@ -1,27 +1,31 @@
-let choices = ['Rock', 'Paper', 'Scissors'];
-let weapons = choices[Math.floor(Math.random()*choices.length)];
-function getComputerChoice(weapons) {
-    return 'The computer chose: ' + weapons
-}
-
-let playerScore = 0
-let computerScore = 0
-let roundWinner = ''
-
-function playRound(playerSelection, getComputerChoice) {
-    if (playerSelection === getComputerChoice) 
-      {
-        para.textContent = `It\'s a tie!!`;
-      } else if (playerSelection === 'Rock' && getComputerChoice === 'Scissors') 
-                (playerSelection === 'Scissors' && getComputerChoice === 'Paper') ||
-                (playerSelection === 'Paper' && getComputerChoice === 'Rock') 
-        {
-        para.textContent = `You won!`;
-        playerScore++
-        roundWinner = `You beat the machine!!`
-        }
-    }
-  const playerSelection = prompt();
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, getComputerChoice));
+function getComputerChoice(string) {
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return 'Rock'
+    case 1:
+      return 'Paper'
+    case 2:
+      return 'Scissors'
+  }
+  //Everything above works
+  
+}function playRound(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice().toLowerCase();
+  playerSelection = playerSelection().toLowerCase();
+  if (computerSelection == playerSelection) {
+    displayResults("Tie game!");
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  )
+      { para.textContent = `You lost.`;
+        computerScore++;  
+      } else { para.textContent = 'You won!!';}
+  }
+  
+const playerSelection = prompt();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, getComputerChoice));
 
